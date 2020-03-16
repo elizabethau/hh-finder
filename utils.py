4bde3dbcbceaeb04a7d2e7487b253b0ef5777071
vagrant <vagrant@vagrant.vm>
2020-03-16 20:51:17 +0000

    new folder

---
+++ utils.py
@@ -0,0 +1,32 @@
+import requests
+
+def send_api_request(user_input):
+
+    url = "https://api.yelp.com/v3/businesses/search?location=" + user_input + "&term =happy hour"
+
+    payload = {}
+    headers = {
+      'Authorization': 'Bearer NTAwKwdS4nSrXJxuv_wSsGmRyLse6bvFW-gECWO4Kvu20lmz8iKfsoA0Oix4MSJElLXw5LCHjgBiCIH8Z7OuEr-jgfy_yFZDzpaenyZvjy0H7BUWxuh62hk0IGdgXnYx'
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+    
+    json_response = response.json()
+
+    return json_response
+
+def send_api_request2(yelp_id):
+
+    url = "https://api.yelp.com/v3/businesses/" + yelp_id
+
+    payload = {}
+    headers = {
+      'Authorization': 'Bearer NTAwKwdS4nSrXJxuv_wSsGmRyLse6bvFW-gECWO4Kvu20lmz8iKfsoA0Oix4MSJElLXw5LCHjgBiCIH8Z7OuEr-jgfy_yFZDzpaenyZvjy0H7BUWxuh62hk0IGdgXnYx'
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+
+    json_response = response.json()
+
+    return json_response
+
